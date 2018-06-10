f72ee6c37c1ac21fa0663854fcb4377bf2afbab1
MaximKudryavtsev <maks07031997@yandex.ru>
2018-06-10 23:29:23 +0300

    поправил run.cmd

--- ds-5/run.cmd
+++ ds-5/run.cmd
@@ -13,7 +13,7 @@ start "TextListener" /d TextListener dotnet TextListener.dll
 
 set file=config\components_config.txt
 for /f "tokens=1,2 delims=:" %%a in (%file%) do (
-for /l %%i in (1, 1, %%b) do start /d %%a dotnet %%a.dll
+for /l %%i in (1, 1, %%b) do start "%%a" /d %%a dotnet %%a.dll
 )
 
 start http://127.0.0.1:5001/home/upload
