5b99e6b9c64f6def05a9a93b161fac24a8a405f0
MaximKudryavtsev <maks07031997@yandex.ru>
2018-06-10 23:27:37 +0300

    ds-5

---
+++ ds-5/run.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Запускаем редис
+for %%i in ("%~dp0\..") do set "parent=%%~fi"
+for %%i in ("%parent%\..") do set "big_parent=%%~fi"
+start /d %big_parent%\Redis redis-server.exe
+
+start "Frontend"  /d Frontend dotnet Frontend.dll
+start "Backend"  /d Backend dotnet Backend.dll 
+start "TextRankCalc"  /d TextRankCalc dotnet TextRankCalc.dll
+start "TextListener" /d TextListener dotnet TextListener.dll
+
+set file=config\components_config.txt
+for /f "tokens=1,2 delims=:" %%a in (%file%) do (
+for /l %%i in (1, 1, %%b) do start /d %%a dotnet %%a.dll
+)
+
+start http://127.0.0.1:5001/home/upload
+
